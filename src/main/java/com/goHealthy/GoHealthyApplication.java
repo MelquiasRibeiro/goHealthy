eac57a207fee14bea55293d94e6b6de614b9f53a
MelquiasRibeiro <melquias10.mr@gmail.com>
2019-10-16 23:24:01 -0300

    projeto criado

---
+++ src/main/java/com/goHealthy/GoHealthyApplication.java
@@ -0,0 +1,13 @@
+package com.goHealthy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoHealthyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoHealthyApplication.class, args);
+	}
+
+}
